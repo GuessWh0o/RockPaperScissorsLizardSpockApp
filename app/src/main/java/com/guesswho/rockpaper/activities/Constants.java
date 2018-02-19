1b7fe77646df6abb007d18450b94f6edf6aba062
GuessWh0o <developerint97@gmail.com>
2018-02-20 00:15:47 +0200

    Modifications on styles, Refactoring code and logics(still continue)

--- app/src/main/java/com/guesswho/rockpaper/activities/Constants.java
+++ app/src/main/java/com/guesswho/rockpaper/activities/Constants.java
@@ -1,8 +1,8 @@
 package com.guesswho.rockpaper.activities;
 
 /**
- * Created by Maksym on 6/26/17.
- * Company: Activaire
+ * Created by GuessWh0o on 18.02.2018.
+ * Email: developerint97@gmail.com
  */
 
 public class Credentials {
@@ -10,4 +10,6 @@ public class Credentials {
     public final static String prefsName = "MySharedPrefs";
     public static String facebookName = "Facebook Name";
     public static String imgUrl = "ImageUrl";
+
+    public static name
 }
---
+++ app/src/main/java/com/guesswho/rockpaper/utils/SharedPrefsUtil.java
@@ -0,0 +1,9 @@
+package com.guesswho.rockpaper.utils;
+
+/**
+ * Created by GuessWh0o on 19.02.2018.
+ * Email: developerint97@gmail.com
+ */
+
+public class SharedPrefsUtil {
+}
